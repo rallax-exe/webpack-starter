a292897cd1da33b2b26dbb85cd7b8dd59b2b55f4
Raul <raul.lara.unam@gmail.com>
2022-07-05 13:51:58 -0500

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+
+
+const HtmlWebPack     = require('html-webpack-plugin');
+const MiniCssExtract  = require("mini-css-extract-plugin");
+const CopyPlugin      = require("copy-webpack-plugin");
+
+module.exports = {
+
+    mode: 'development',
+
+    output: {
+        clean: true
+    },
+    
+    module: {
+        rules: [
+
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+
+            {
+                test: /styles.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+
+        
+
+        ]
+    },
+
+    optimization: {},
+    
+    plugins: [
+        new HtmlWebPack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html'
+            template: './src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+              {from: 'src/assets/', to: 'assets/'}
+            ]
+        }),
+
+    ],
+}
